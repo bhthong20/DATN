60679ab44e4bd3d59508f06e55b300a7afb3aa71
Hoang Tu <146807301+hoangtu89@users.noreply.github.com>
2025-01-03 15:42:35 +0700

    fix API GHTK

--- webbangiay/src/main/java/com/example/demo/util/CallApiGiaHangTietKiem.java
+++ webbangiay/src/main/java/com/example/demo/util/CallApiGiaHangTietKiem.java
@@ -21,7 +21,7 @@ public class CallApiGiaHangTietKiem {
     public Map<String, Object> getPhiGiaoHang(String province, String district) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String apiUrl = "https://services.giaohangtietkiem.vn/services/shipment/fee?pick_province=Thành phố Hà Nội&pick_district=Quận Từ Liêm&province=" + province + "&district=" + district + "&address=P.503 tòa nhà Auu Việt, số 1 Lê Đức Thọ&weight=0&transport=fly&deliver_option=xteam";
+            String apiUrl = "https://services.giaohangtietkiem.vn/services/shipment/fee?pick_province=Thành phố Hà Nội&pick_district=Quận Từ Liêm&province=" + province + "&district=" + district + "&address=P.503 tòa nhà Auu Việt, số 1 Lê Đức Thọ&weight=1000&transport=road&deliver_option=none";
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.set("Token", "8b3517821b6b32db7571dd0dd0af734180384757");
